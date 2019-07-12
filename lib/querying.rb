1a8f9a3c6cf3b66582e650d0670c2a8c8c62dab4
Trish Ogen <patriciaogen@gmail.com>
2019-07-12 01:37:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "select s.title
+  from series s
+  join characters c
+  s.id = c.series_id
+  where c.species = 'human'
+  group by s.id;
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
