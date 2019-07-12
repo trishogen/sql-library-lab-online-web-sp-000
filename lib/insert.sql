5fe4eb6c6eb5f7bec04c68b2b926fa4153721e94
Trish Ogen <patriciaogen@gmail.com>
2019-07-12 00:27:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,9 @@
-INSERT INTO series (title, author_id, subgenre_id) values ('The Kingkiller Chronicles', NULL, NULL);
+INSERT INTO series (title, author_id, subgenre_id) values ('The Kingkiller Chronicle', NULL, NULL);
 INSERT INTO series (title, author_id, subgenre_id) values ('The Wheel of Time', NULL, NULL);
+
+INSERT INTO books (title, year, series_id) values ('The Name of the Wind', 2007, 1);
+INSERT INTO books (title, year, series_id) values ('The Wise Mans Fear', 2011, 1);
+INSERT INTO books (title, year, series_id) values ('TBD', NULL , 1);
+INSERT INTO books (title, year, series_id) values ('The Eye of the World', 1990, 2);
+INSERT INTO books (title, year, series_id) values ('The Great Hunt', 1990, 2);
+INSERT INTO books (title, year, series_id) values ('The Dragon Reborn', 1991, 2);
