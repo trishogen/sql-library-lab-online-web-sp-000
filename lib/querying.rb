d4ac08dd41fed100a3c224a76e834cf883bd1969
Trish Ogen <patriciaogen@gmail.com>
2019-07-12 01:32:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select species, count(species)
+  from characters
+  group by species
+  order by count(species)
+  limit 1"
 end
 
 def select_name_and_series_subgenres_of_authors
