77fa9a9d6811f9d2c32b6d7437701f36ed3ffdc4
Trish Ogen <patriciaogen@gmail.com>
2019-07-12 00:23:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
-INSERT INTO 
+INSERT INTO series (title, author_id, subgenre_id) values ('The Kingkiller Chronicles', NULL, NULL);
+INSERT INTO series (title, author_id, subgenre_id) values ('The Wheel of Time', NULL, NULL);
