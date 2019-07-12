b05cce3bc1454e3d32cbc18b626be85481bb27c8
Trish Ogen <patriciaogen@gmail.com>
2019-07-12 01:28:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "select name, max(motto)
+  "select name, max(length(motto))
   from characters;"
 end
 
